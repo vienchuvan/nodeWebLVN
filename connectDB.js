2969d8eafdd52ccaaf0b7556c6ad7b847c498261
vienit <vienchelsea325@gmail.com>
2026-05-27 22:26:23 +0700

    vien

--- connectDB.js
+++ connectDB.js
@@ -1,35 +1,15 @@
 const mysql = require('mysql2/promise');
 
-const connection = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
-   user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT,
- 
- 
-    waitForConnections: true,
-    connectionLimit: 10, 
-    queueLimit: 0
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-connection.getConnection((err, connection) => {
-    if (err) {
-        console.error('Lỗi kết nối MySQL:', err);
-        return;
-    }
-    console.log('✅ Kết nối MySQL thành công!');
-    connection.release(); // Giải phóng kết nối sau khi kiểm tra
-});
-
-// connection.connect((err) => {
-//   if (err) {
-//     console.log('Kết nối thất bại:', err);
-//     return;
-//   }
-
-//   console.log('MySQL Connected');
-// });
-
-module.exports = connection;
+module.exports = pool;
