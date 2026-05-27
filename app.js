ab1b5bdf278a2fc95b17e4bc8cc96d1a6d7cc659
vienit <vienchelsea325@gmail.com>
2026-05-27 22:08:10 +0700

    vien

--- app.js
+++ app.js
@@ -7,24 +7,23 @@ const path = require('path');
 const fs = require('fs');
 
 app.use(cors());
+
 const webRoutes = require('./routes/index');
-const db = require('./connectDB')
+const db = require('./connectDB');
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 if (!fs.existsSync('uploads')) {
   fs.mkdirSync('uploads');
 }
 
-// Serve static files from uploads folder
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
-// routes
 app.use('/', webRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running`);
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
 });
