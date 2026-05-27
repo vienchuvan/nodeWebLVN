e98c521db86cea2c8d89cde825b1e1704d8bd153
vienit <vienchelsea325@gmail.com>
2026-05-27 22:56:57 +0700

    vien

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-  res.json({
-    ok: true
-  });
-});
+const homeController = require('../controller/homeController');
+
+// route home
+router.get('/', homeController.home);
+
+// menu
+router.get('/menu', homeController.menu);
+router.get('/lang-menu', homeController.langMenu);
+router.post('/set-menu', homeController.setMenu);
+
+router.post('/set-banner', homeController.bannerHome);
+router.post('/setting-home', homeController.setGeneralSettings);
+
+
+router.post(
+  "/sidebar-menu",
+  homeController.getSidebarMenu
+);
+
+// route about
+router.get('/about', homeController.about);
+
+// route api
+router.get('/api/users', homeController.users);
+
+// contacts
+router.post(
+  '/quantri/contacts',
+  require('../controller/contact').contactController
+);
+
+// articles
+router.post(
+  '/quantri/baiviet',
+  ...require('../controller/quantribaiviet').articleController
+);
 
 module.exports = router;
